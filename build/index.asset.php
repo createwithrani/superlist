bca611f39ea7eae49629bb980a7d2ef50926edfa
Aurooba Ahmed <hey@aurooba.com>
2022-01-27 16:38:28 -0700

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e1ff5ffa14c37763a2843feab68aa66b');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bd39bcccf5cce3ec9bff0f731f7b29e5');
