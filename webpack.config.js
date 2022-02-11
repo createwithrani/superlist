107a3885641726bbd32a37d1bd403578a135d9b7
Aurooba Ahmed <hey@aurooba.com>
2022-02-11 12:49:41 -0700

    update build flow to use latest wordpress/scripts

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-
-module.exports = {
-	...defaultConfig,
-	entry: {
-		"superlist-item": "./src/superlist-item/index.js",
-		superlist: "./src/superlist/index.js",
-	},
-};
