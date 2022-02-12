f2ece32911e40ab194b689571de0f50633c1537c
Aurooba Ahmed <hey@aurooba.com>
2022-02-11 20:55:10 -0700

    a little export cleanup

--- src/superlist/index.js
+++ src/superlist/index.js
@@ -21,8 +21,8 @@ import "./style.scss";
 import metadata from "./block.json";
 import edit from "./edit";
 import save from "./save";
-import { example } from "./example";
-import { transforms } from "./transforms";
+import example from "./example";
+import transforms from "./transforms";
 import { SuperList as icon } from "./icons";
 
 /**
